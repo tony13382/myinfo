58af3eab6140577a49e568fccf1d653cd68a380a
Meow <ttsmcpe@gmail.com>
2021-11-28 11:07:48 +0800

    fix flex

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,2 +1,1 @@
 function toTop() {document.getElementById('nav').scrollIntoView({behavior:"smooth"})}
-function scrollTo(idName) {document.getElementById(idName).scrollIntoView({behavior:"smooth"})}
